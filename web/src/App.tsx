ef90ca18189f7003658f69c0280edd731f280d5c
AnaSpinetti <ana-spinetti@hotmail.com>
2022-09-16 17:43:36 -0300

    update eSports

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       </div>
 
     <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden"></div>
-      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center self-stretch">
         <div>
           <strong className="text-2xl text-white font-black block">Não encontrou o seu duo?</strong>
           <span className="text-zinc-400 block">Publique o seu anúncio para encontrar novos players</span>
